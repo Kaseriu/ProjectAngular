42fc9bbafe0d08cc917f2bb865f3e0ef195361b1
Adem Mrizak <adem.mrizak@gmail.com>
2021-06-09 16:54:18 +0200

    Display date

--- src/app/arena/arena.component.ts
+++ src/app/arena/arena.component.ts
@@ -8,18 +8,28 @@ import {BattleStateService} from '../services/battle-state/battle-state.service'
 })
 export class ArenaComponent {
 
+  startDate: number = null;
+
   constructor(public battleStateService: BattleStateService) {
   }
 
   handleClick(): void {
+    this.displayDate();
+
     if (this.battleStateService.gamePaused) {
       this.battleStateService.gamePaused = false;
-      this.battleStateService.fight().then(a => {
+      this.battleStateService.fight().then(() => {
       });
     } else {
       this.battleStateService.gamePaused = true;
       clearInterval(this.battleStateService.battleIntervalId);
       this.battleStateService.battleIntervalId = null;
+    }
+  }
+
+  displayDate(): void {
+    if (this.startDate === null) {
+      this.startDate = Date.now();
     }
   }
 }
