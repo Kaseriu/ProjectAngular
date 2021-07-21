7a5c8b1280b31e300c2687a6c7475d555cb9587f
Adem Mrizak <adem.mrizak@gmail.com>
2021-07-21 16:39:02 +0200

    Exercice 16

--- src/app/pokemon-selection/pokemon-selection.component.ts
+++ src/app/pokemon-selection/pokemon-selection.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {RoutingService} from '../services/routing/routing.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
     selector: 'app-pokemon-selection',
@@ -7,13 +8,16 @@ import {RoutingService} from '../services/routing/routing.service';
     styleUrls: ['./pokemon-selection.component.css']
 })
 export class PokemonSelectionComponent implements OnInit {
-    pokemon1: string;
-    pokemon2: string;
-
     constructor(public routingService: RoutingService) {
     }
 
     ngOnInit(): void {
     }
 
+    async onSubmit(f: NgForm): Promise<void> {
+        const {pokemon1, pokemon2} = f.value;
+        if (!!pokemon1 && !!pokemon2) {
+            await this.routingService.goToBattle(pokemon1, pokemon2);
+        }
+    }
 }
